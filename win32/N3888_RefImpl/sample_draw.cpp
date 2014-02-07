59448de381277e3ddf25ce9af48110847f200f0c
Michael B. McLaughlin <mikebmcl@gmail.com>
2014-02-07 00:42:36 -0500

    Simple animated bubble sort visualization.

--- win32/N3888_RefImpl/sample_draw.cpp
+++ win32/N3888_RefImpl/sample_draw.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 using namespace std::experimental::drawing;
@@ -38,27 +39,36 @@ vector<vector<int>> init_sort_steps(int count) {
 	return result;
 }
 
-void sample_draw::operator()(::std::experimental::drawing::context& ctxt, double /*elapsedTimeInMilliseconds*/) {
+void sample_draw::operator()(context& ctxt, double elapsedTimeInMilliseconds) {
+	static double timer = 0.0;
+	const double phaseTime = 1250.0;
 	const int elementCount = 10; // height
-	static vector<vector<int>> vec = init_sort_steps(elementCount);
-	int stepCount = vec.size(); // width
+	const static auto vec = init_sort_steps(elementCount);
+	const int stepCount = vec.size(); // width
+	const auto x = min(static_cast<int>(timer / phaseTime), stepCount - 1);
 	ctxt.set_source_rgb(0.392156899, 0.5843137503, 0.9294118285);
-	ctxt.paint();
-	double left, top, right, bottom, unitWidth, unitHeight, beginX, beginY;
+	ctxt.paint(); // Paint background.
+	double left, top, right, bottom, beginX;
 	ctxt.clip_extents(left, top, right, bottom);
-	unitWidth = trunc(((right - left) * 0.8) / stepCount);
-	unitHeight = trunc(((bottom - top) * 0.8) / elementCount);
+	const auto radius = trunc(min((right - left) * 0.8 / elementCount, (bottom - top) + 120.0) / 2.0);
 	beginX = trunc((right - left) * 0.05);
-	beginY = trunc((bottom - top) * 0.05);
+	const double y = trunc((bottom - top) * 0.5);
 	const double two_pi = 3.1415926535897932 * 2.0;
-	for (int x = 0; x < stepCount; ++x) {
-		for (int y = 0; y < elementCount; ++y) {
-			//ctxt.move_to(beginX * x, beginY * y);
-			const auto radius = trunc(min(unitWidth, unitHeight) / 2.0);
-			ctxt.arc(radius * x * 2.0 + radius + beginX + (4.0 * x), radius * y * 2.0 + radius + beginY + (4.0 * y), radius, 0.0, two_pi);
-			double greyColor = 1.0 - (vec[x][y] / (elementCount - 1.0));
-			ctxt.set_source_rgb(greyColor, greyColor, greyColor);
-			ctxt.fill();
-		}
+	ctxt.move_to(20.0, 50.0);
+	ctxt.set_source_rgb(1.0, 1.0, 1.0);
+	ctxt.select_font_face("Segoe UI", font_slant::normal, font_weight::normal);
+	ctxt.set_font_size(40.0);
+	ctxt.show_text(string("Phase ").append(to_string(x + 1)).c_str());
+	for (int i = 0; i < elementCount; ++i) {
+		ctxt.arc(radius * i * 2.0 + radius + beginX + (4.0 * i), y, radius, 0.0, two_pi);
+		double greyColor = 1.0 - (vec[x][i] / (elementCount - 1.0));
+		ctxt.set_source_rgb(greyColor, greyColor, greyColor);
+		ctxt.fill();
 	}
+	timer += elapsedTimeInMilliseconds;
+	timer = timer > phaseTime * (stepCount + 3) ? 0.0 : timer;
 }
+
+//const auto radius = trunc(min(
+//	((right - left) * 0.8) / stepCount,
+//	((bottom - top) * 0.8) / elementCount) / 2.0);
