0d497bfe3ffe1b74e12ebf918d955df023e81ef5
Michael B. McLaughlin <mikebmcl@gmail.com>
2014-02-07 12:13:11 -0500

    Cleanups, condensing, and parameter tweaking.

--- win32/N3888_RefImpl/sample_draw.cpp
+++ win32/N3888_RefImpl/sample_draw.cpp
@@ -8,14 +8,14 @@
 using namespace std;
 using namespace std::experimental::drawing;
 
-vector<vector<int>> init_sort_steps(int count) {
+vector<vector<int>> init_sort_steps(int count, unsigned long mtSeed = 1009UL) {
 	vector<vector<int>> result;
-	result.push_back([count]() {
+	result.push_back([count, mtSeed]() {
 		vector<int> init;
 		for (int i = 0; i < count; ++i) {
 			init.push_back(i);
 		}
-		mt19937 rng(1009);
+		mt19937 rng(mtSeed);
 		shuffle(begin(init), end(init), rng);
 		return init;
 	}());
@@ -38,49 +38,42 @@ vector<vector<int>> init_sort_steps(int count) {
 	}
 	return result;
 }
-// TODO: Lerp between phases with a pause at the end before beginning the next phase. auto it_index = it - begin(vec);
+
 void sample_draw::operator()(context& ctxt, double elapsedTimeInMilliseconds) {
 	static double timer = 0.0;
-	const double power = 3.0;
-	const double lerpTime = 1500.0;
-	const double phaseTime = lerpTime + 500.0;
+	const double power = 3.0, lerpTime = 1250.0, phaseTime = lerpTime + 500.0, two_pi = 6.2831853071795864;
 	const double normalizedTime = min(fmod(timer, phaseTime) / lerpTime, 1.0);
 	const double adjustment = (normalizedTime < 0.5) ? pow(normalizedTime * 2.0, power) / 2.0 :
 		((1.0 - pow(1.0 - ((normalizedTime - 0.5) * 2.0), power)) * 0.5) + 0.5;
-	const int elementCount = 10; // height
+	const int elementCount = 12;
 	const static auto vec = init_sort_steps(elementCount);
-	const int stepCount = vec.size(); // width
-	const auto x = min(static_cast<int>(timer / phaseTime), stepCount - 1);
+	const int phaseCount = static_cast<int>(vec.size()), x = min(static_cast<int>(timer / phaseTime), phaseCount - 1);
 	ctxt.set_source_rgb(0.392156899, 0.5843137503, 0.9294118285);
 	ctxt.paint(); // Paint background.
-	double left, top, right, bottom, beginX;
+	double left, top, right, bottom;
 	ctxt.clip_extents(left, top, right, bottom);
-	const auto radius = trunc(min((right - left) * 0.8 / elementCount, (bottom - top) + 120.0) / 2.0);
-	beginX = trunc((right - left) * 0.05);
-	const double y = trunc((bottom - top) * 0.5);
-	const double two_pi = 3.1415926535897932 * 2.0;
-	ctxt.move_to(20.0, 50.0);
+	const double radius = trunc(min((right - left) * 0.8 / elementCount, (bottom - top) + 120.0) / 2.0);
+	const double beginX = trunc((right - left) * 0.1), y = trunc((bottom - top) * 0.5);
+	ctxt.move_to(beginX, 50.0);
 	ctxt.set_source_rgb(1.0, 1.0, 1.0);
 	ctxt.select_font_face("Segoe UI", font_slant::normal, font_weight::normal);
 	ctxt.set_font_size(40.0);
 	ctxt.show_text(string("Phase ").append(to_string(x + 1)).c_str());
 	for (int i = 0; i < elementCount; ++i) {
 		const auto currVal = vec[x][i];
-		if (x < stepCount - 1) {
-			const auto bi = find(begin(vec[x + 1]), end(vec[x + 1]), currVal) - begin(vec[x + 1]);
-			const auto ax = radius * i * 2.0 + radius + beginX + (4.0 * i);
-			const auto bx = radius * bi * 2.0 + radius + beginX + (4.0 * bi);
-			const auto yr = y - ((bi == i ? 0.0 : (radius * 4.0 * (normalizedTime < 0.5 ? normalizedTime : 1.0 - normalizedTime)))
+		if (x < phaseCount - 1) {
+			const auto i2 = find(begin(vec[x + 1]), end(vec[x + 1]), currVal) - begin(vec[x + 1]);
+			const auto x1r = radius * i * 2.0 + radius + beginX, x2r = radius * i2 * 2.0 + radius + beginX;
+			const auto yr = y - ((i2 == i ? 0.0 : (radius * 4.0 * (normalizedTime < 0.5 ? normalizedTime : 1.0 - normalizedTime)))
 				* (i % 2 == 1 ? 1.0 : -1.0));
-			ctxt.arc((bx - ax) * adjustment + ax, yr, radius, 0.0, two_pi);
+			ctxt.arc(trunc((x2r - x1r) * adjustment + x1r), trunc(yr), radius - 3.0, 0.0, two_pi);
 		}
 		else {
 			ctxt.arc(radius * i * 2.0 + radius + beginX + (4.0 * i), y, radius, 0.0, two_pi);
 		}
-		double greyColor = 1.0 - (vec[x][i] / (elementCount - 1.0));
+		double greyColor = 1.0 - (currVal / (elementCount - 1.0));
 		ctxt.set_source_rgb(greyColor, greyColor, greyColor);
 		ctxt.fill();
 	}
-	timer += elapsedTimeInMilliseconds;
-	timer = (timer > phaseTime * (stepCount + 3)) ? 0.0 : timer;
+	timer = (timer > phaseTime * (phaseCount + 2)) ? 0.0 : timer + elapsedTimeInMilliseconds;
 }
