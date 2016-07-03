ef6a8bae437e72ef82131c268c1326c7cd997f1a
Michael B. McLaughlin <mikebmcl@gmail.com>
2016-07-03 19:13:36 -0400

    Fixed the default font matrix in font_resource_factory to reflect the correct font size. Minor change to the vcxproj to allow for a VS 2015 code map. Edited entrypoint.cpp to show the canonical "hello world" example. Edited the run-configure-with-* for GCC to use "-std=c++14" instead of "-std=c++1y" in response to issue #32 (note: not yet tested by me).

--- tests/sample-draw/entrypoint.cpp
+++ tests/sample-draw/entrypoint.cpp
@@ -8,23 +8,23 @@ using namespace std::chrono;
 using namespace std::experimental::io2d;
 
 int main() {
-	//auto ds = make_display_surface(640, 480, format::argb32);
-	//ds.draw_callback([](display_surface& ds) {
-	//	ds.paint(rgba_color::cornflower_blue());
-	//	ds.render_text("Hello world!", { 50.0, 50.0 }, rgba_color::black());
-	//});
-	//return ds.show();
-
-	assert(steady_clock::is_steady);
-	auto ds = make_display_surface(1280, 720, format::argb32, scaling::letterbox, refresh_rate::fixed, 30.0);
-	auto radialFactory = radial_brush_factory({ 0.0, 0.0 }, 0.0, { 0.0, 0.0 }, 50.0);
-	radialFactory.add_color_stop(0.0, rgba_color::orange());
-	radialFactory.add_color_stop(1.0, rgba_color::aquamarine());
-	auto letterboxBrush = brush(radialFactory);
-	letterboxBrush.extend(extend::repeat);
-	ds.letterbox_brush(letterboxBrush);
-	sample_draw sd;
-	ds.draw_callback(sd);
+	auto ds = make_display_surface(640, 480, format::argb32);
+	ds.draw_callback([](display_surface& ds) {
+		ds.paint(rgba_color::firebrick());
+		ds.render_text("Hello world!", { 50.0, 50.0 }, rgba_color::aqua());
+	});
 	return ds.show();
+
+	//assert(steady_clock::is_steady);
+	//auto ds = make_display_surface(1280, 720, format::argb32, scaling::letterbox, refresh_rate::fixed, 30.0);
+	//auto radialFactory = radial_brush_factory({ 0.0, 0.0 }, 0.0, { 0.0, 0.0 }, 50.0);
+	//radialFactory.add_color_stop(0.0, rgba_color::orange());
+	//radialFactory.add_color_stop(1.0, rgba_color::aquamarine());
+	//auto letterboxBrush = brush(radialFactory);
+	//letterboxBrush.extend(extend::repeat);
+	//ds.letterbox_brush(letterboxBrush);
+	//sample_draw sd;
+	//ds.draw_callback(sd);
+	//return ds.show();
 }
 
