38e8e1917ece9690d95f4632a26f3892069f4bff
Guy Davidson <guy@hatcat.com>
2017-06-28 18:13:17 +0100

    Expressed tau in terms of two_pi rather than as a constant Made eighth_tau constexpr

--- N3888_RefImpl/RocksInSpace/Maths.h
+++ N3888_RefImpl/RocksInSpace/Maths.h
@@ -19,9 +19,9 @@ namespace asteroids
 	};
 
 	template <class T>
-	constexpr T tau = T(6.283185307179586476925286766559L);
+	constexpr T tau = T(std::experimental::io2d::two_pi<T>);
 	template <class T>
-	const T eighth_tau = T(tau<T> / T(8.0L));
+	constexpr T eighth_tau = T(tau<T> / T(8.0L));
 
 	vector_2d rotate(const vector_2d& point, float theta, const vector_2d& origin);
 	bool intersects(vector_2d, vector_2d, vector_2d, vector_2d);
