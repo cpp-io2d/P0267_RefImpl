ebfddf37530817c7be4f030b2362ee06c46c9625
Michael B. McLaughlin <mikebmcl@gmail.com>
2017-10-25 06:52:53 -0400

    run-configure-with-gcc8-macports.sh no longer builds with -g by default. Instead, you can run it and pass it flags like -g which it will use (along with certain default flags) as part of CXXFLAGS when running configure.

--- run-configure-with-gcc8-macports.sh
+++ run-configure-with-gcc8-macports.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-echo "Running './configure CC=gcc-mp-8 CFLAGS=\"-g\" CXX=g++-mp-8 CXXFLAGS=\"-g -std=c++1z -Wall -Wno-float-equal -Wno-padded\" LDFLAGS=\"-L/opt/local/lib\" CPPFLAGS=\"-isystem/opt/local/include\""
-./configure CC=gcc-mp-8 CFLAGS="-g" CXX=g++-mp-8 CXXFLAGS="-g -std=c++1z -Wall -Wno-float-equal -Wno-padded" LDFLAGS="-L/opt/local/lib" CPPFLAGS="-isystem/opt/local/include"
+CXXFLAGSVALS="$@"
+CXXFLAGSVALS+=" -std=c++1z -Wall -Wno-float-equal -Wno-padded"
+
+echo "Running './configure CC=gcc-mp-8 CFLAGS=\"-g\" CXX=g++-mp-8 CXXFLAGS=\"${CXXFLAGSVALS}\" LDFLAGS=\"-L/opt/local/lib\" CPPFLAGS=\"-isystem/opt/local/include\""
+#echo "Running './configure CC=gcc-mp-8 CFLAGS=\"-g\" CXX=g++-mp-8 CXXFLAGS=\"-g -std=c++1z -Wall -Wno-float-equal -Wno-padded\" LDFLAGS=\"-L/opt/local/lib\" CPPFLAGS=\"-isystem/opt/local/include\""
+
+./configure CC=gcc-mp-8 CFLAGS="-g" CXX=g++-mp-8 CXXFLAGS="${CXXFLAGSVALS}"  LDFLAGS="-L/opt/local/lib" CPPFLAGS="-isystem/opt/local/include"
+#./configure CC=gcc-mp-8 CFLAGS="-g" CXX=g++-mp-8 CXXFLAGS="-g -std=c++1z -Wall -Wno-float-equal -Wno-padded" LDFLAGS="-L/opt/local/lib" CPPFLAGS="-isystem/opt/local/include"
