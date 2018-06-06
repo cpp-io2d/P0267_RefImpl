5af92f606400a6ffcdd5f2b9bf050a07f6ebba7d
David Ludwig <dludwig@pobox.com>
2018-06-06 15:05:05 -0400

    modified ColorFill sample, to print info on thrown + uncaught exceptions

--- P0267_RefImpl/Samples/ColorFill/main.cpp
+++ P0267_RefImpl/Samples/ColorFill/main.cpp
@@ -6,18 +6,25 @@ using namespace std::experimental;
 using namespace std::experimental::io2d;
 
 static output_surface & display() {
-    static auto surface = output_surface{500, 500, format::argb32, scaling::none, refresh_style::fixed, 30.f};
+    static auto surface = output_surface{500, 500, format::argb32, scaling::none, refresh_style::as_needed, 30.f};
     return surface;
 }
 
 int main(int argc, const char * argv[]) {
-    display().size_change_callback([&](output_surface& surface){
-        surface.dimensions(surface.display_dimensions());
-    });
-    display().draw_callback([&](auto& surface) {
-        brush greenBrush{rgba_color::green};
-        surface.paint(greenBrush);
-    });
-    display().begin_show();
+    try {
+        auto & d = display();
+        d.size_change_callback([&](output_surface& surface){
+            surface.dimensions(surface.display_dimensions());
+        });
+        d.draw_callback([&](auto& surface) {
+            brush mainColor{rgba_color::green};
+            surface.paint(mainColor);
+        });
+        d.begin_show();
+    } catch (const std::exception & ex) {
+        printf("std::exception caught, what=\"%s\"\n", ex.what());
+    } catch (...) {
+        printf("unknown exception caught\n");
+    }
     return 0;
 }
