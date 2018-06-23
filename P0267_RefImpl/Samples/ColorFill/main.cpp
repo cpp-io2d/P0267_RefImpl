6591b81e77f2a1c35135b3a73e418b07f0a72c0b
David Ludwig <dludwig@pobox.com>
2018-06-23 00:50:16 -0400

    cleanup in ColorFill

--- P0267_RefImpl/Samples/ColorFill/main.cpp
+++ P0267_RefImpl/Samples/ColorFill/main.cpp
@@ -1,27 +1,25 @@
-#include <io2d.h>
-#include <mutex>
 
-using namespace std;
-using namespace std::experimental;
+#include <io2d.h>
+
 using namespace std::experimental::io2d;
 
 static output_surface & display() {
-    static auto surface = output_surface{500, 500, format::argb32, scaling::none, refresh_style::as_needed, 30.f};
+    static auto surface = output_surface {512, 512, format::argb32, scaling::none, refresh_style::as_needed, 30.f};
     return surface;
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     try {
-        auto & d = display();
-        d.size_change_callback([&](output_surface& surface){
+        auto &d = display();
+        d.size_change_callback([&](output_surface &surface) {
             surface.dimensions(surface.display_dimensions());
         });
-        d.draw_callback([&](auto& surface) {
-            brush mainColor{rgba_color::green};
+        d.draw_callback([&](auto &surface) {
+            brush mainColor {rgba_color::green};
             surface.paint(mainColor);
         });
         d.begin_show();
-    } catch (const std::exception & ex) {
+    } catch (const std::exception &ex) {
         printf("std::exception caught, what=\"%s\"\n", ex.what());
     } catch (...) {
         printf("unknown exception caught\n");
