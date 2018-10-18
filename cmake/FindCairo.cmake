7336da1e5a92bc20d905b4a9aa522417e7ea1191
David Ludwig <dludwig@pobox.com>
2018-10-18 12:48:13 -0400

    make FindCairo.cmake export an IMPORTED target
    FindCairo.cmake will now create an 'IMPORTED' CMake target, with
    build settings assigned to it via CMake target-properties.
    
    This change also, somewhat inadvertently, fixes a bug whereby vcpkg
    wasn't installing Cairo's .dll file alongside Debug-built .exe files,
    as the Release/non-Debug version of Cairo was getting linked to.
    vcpkg adds a post-link step to executables to install these .dll files.
    Debug-built io2d apps were linking to the Release copy of Cairo, which
    was confusing vcpkg.  In more detail, this involved a vcpkg script,
    "<vcpkg-root>\scripts\buildsystems\msbuild\applocal.ps1", only looking
    in "<vcpkg-root>\debug\bin\" for .dll files to copy, as opposed to
    either "<vcpkg-root>\debug\bin\" or "<vcpkg-root>\bin\".

--- cmake/FindCairo.cmake
+++ cmake/FindCairo.cmake
@@ -43,6 +43,39 @@ find_library(CAIRO_LIBRARIES
     HINTS ${PC_CAIRO_LIBDIR}
           ${PC_CAIRO_LIBRARY_DIRS}
 )
+find_library(CAIRO_LIBRARIES_DEBUG
+    NAMES cairod
+    HINTS ${PC_CAIRO_LIBDIR}
+          ${PC_CAIRO_LIBRARY_DIRS}
+)
+
+if (CAIRO_INCLUDE_DIRS)
+    set(_CAIRO_PATH ${CAIRO_INCLUDE_DIRS})
+    while(TRUE)
+        get_filename_component(_CAIRO_PATH_PART ${_CAIRO_PATH} NAME)
+        string(TOLOWER ${_CAIRO_PATH_PART} _CAIRO_PATH_PART)
+        if (${_CAIRO_PATH_PART} STREQUAL "cairo" OR ${_CAIRO_PATH_PART} STREQUAL "include")
+            get_filename_component(_CAIRO_PATH ${_CAIRO_PATH} DIRECTORY)
+            continue()
+        endif()
+        if (NOT (${_CAIRO_PATH} STREQUAL ""))
+            set(CAIRO_PATH ${_CAIRO_PATH})
+            set(CAIRO_PATH ${CAIRO_PATH} PARENT_SCOPE)
+        endif()
+        break()
+    endwhile()
+endif()
+
+find_file(CAIRO_DLL
+    NAMES cairo.dll
+    HINTS ${CAIRO_PATH}
+    PATH_SUFFIXES bin
+)
+find_file(CAIRO_DLL_DEBUG
+    NAMES cairod.dll
+    HINTS ${CAIRO_PATH}
+    PATH_SUFFIXES debug/bin
+)
 
 if (CAIRO_INCLUDE_DIRS)
     if (EXISTS "${CAIRO_INCLUDE_DIRS}/cairo-version.h")
@@ -72,4 +105,52 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(Cairo REQUIRED_VARS CAIRO_INCLUDE_DIRS CAIRO_L
 mark_as_advanced(
     CAIRO_INCLUDE_DIRS
     CAIRO_LIBRARIES
+    CAIRO_LIBRARIES_DEBUG
 )
+
+# Create CMake targets
+if (CAIRO_FOUND AND NOT TARGET Cairo::Cairo)
+    if (CAIRO_DLL)
+        # Not using 'SHARED' when Cairo is available through a .dll can
+        # cause build issues with MSVC, at least when trying to link against
+        # a vcpkg-provided copy of "cairod".
+        add_library(Cairo::Cairo SHARED IMPORTED)
+    else()
+        add_library(Cairo::Cairo UNKNOWN IMPORTED)
+    endif()
+
+    set_target_properties(Cairo::Cairo PROPERTIES
+        IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+        INTERFACE_INCLUDE_DIRECTORIES ${CAIRO_INCLUDE_DIRS}
+    )
+
+    if(CAIRO_DLL)
+        # When using a .dll, the location of *both( the .dll file, and its .lib,
+        # needs to be specified to CMake.  The path to the .dll goes into
+        # IMPORTED_LOCATION(_*), whereas the path to the .lib goes into
+        # IMPORTED_IMPLIB(_*).
+        set_target_properties(Cairo::Cairo PROPERTIES
+            IMPORTED_LOCATION ${CAIRO_DLL}
+            IMPORTED_IMPLIB ${CAIRO_LIBRARIES}
+        )
+        if (CAIRO_DLL_DEBUG)
+            set_target_properties(Cairo::Cairo PROPERTIES
+                IMPORTED_LOCATION_DEBUG ${CAIRO_DLL_DEBUG}
+            )
+        endif()
+        if (CAIRO_LIBRARIES_DEBUG)
+            set_target_properties(Cairo::Cairo PROPERTIES
+                IMPORTED_IMPLIB_DEBUG ${CAIRO_LIBRARIES_DEBUG}
+            )
+        endif()
+    else()
+        set_target_properties(Cairo::Cairo PROPERTIES
+            IMPORTED_LOCATION ${CAIRO_LIBRARIES}
+        )
+        if (CAIRO_LIBRARIES_DEBUG)
+            set_target_properties(Cairo::Cairo PROPERTIES
+                IMPORTED_LOCATION_DEBUG ${CAIRO_LIBRARIES_DEBUG}
+            )
+        endif()
+    endif()
+endif()
